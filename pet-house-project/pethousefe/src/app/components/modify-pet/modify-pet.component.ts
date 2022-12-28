36e3e3570a55cd81e407c1b834b1c39223b8b0d8
Tudor-Andrei <thenewxdon@gmail.com>
2022-12-28 14:22:25 +0100

    aggiunto modify-pet

---
+++ pet-house-project/pethousefe/src/app/components/modify-pet/modify-pet.component.ts
@@ -0,0 +1,87 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Animal } from 'src/app/model/Animal';
+import { AnimalService } from 'src/app/service/animal.service';
+
+@Component({
+  selector: 'app-modify-pet',
+  templateUrl: './modify-pet.component.html',
+  styleUrls: ['./modify-pet.component.css']
+})
+export class ModifyPetComponent implements OnInit {
+  animal: Animal = new Animal();
+  @ViewChild('modifyform') addForm!: NgForm;
+  name!: string;
+  age!: number;
+  kind!: string;
+  owner!: string;
+  phone!: string;
+  imageUrl!: string;
+  idAnimal: number = +this.route.snapshot.paramMap.get('id')!;
+  constructor(private animalService: AnimalService,
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.animal = new Animal();
+    console.log(this.idAnimal);
+    this.loadAnimal(this.idAnimal);
+    console.log('Animal Id: ' + this.animal.id);
+    console.log('Animal Name: ' + this.animal.name);
+  }
+
+  loadAnimal(id: number){
+    this.animalService.getAnimal(id).subscribe(
+      (response: Animal) => {
+      this.animal = response;
+      console.log('animal', response);
+    }, error => {
+
+    }
+
+    )
+  }
+
+  deleteAnimal() {
+    if (confirm("Do you want to delete this pet?") == true) {
+      this.animalService.deleteAnimal(this.animal.id).subscribe(
+        response => {
+          console.log('eliminato');
+          console.log(response);
+        }, error => {
+          console.log(error);
+        }
+      )
+      setTimeout(() =>{this.router.navigate(["/"])}, 500);
+      
+    }
+  }
+
+  updateAnimal() {
+    this.animal.name = this.name;
+    this.animal.age = this.age;
+    this.animal.kind = this.kind;
+    this.animal.owner = this.owner;
+    this.animal.phone = this.phone;
+    this.animal.imageUrl = this.imageUrl;
+
+    this.animalService.updateAnimal(this.animal).subscribe(data => {
+
+      alert("Successful modifing");
+      this.router.navigate([`/edit/${this.animal.id}`]);
+    },
+    error => {
+      console.log(error);
+      alert("Error in process");
+    })
+    
+  }
+
+  onSubmit() {
+    if (confirm("Do you really want to modify this pet?")) {
+      this.updateAnimal;
+    }
+    
+  }
+}
